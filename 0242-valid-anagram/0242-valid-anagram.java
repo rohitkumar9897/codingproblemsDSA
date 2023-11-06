811311587cfbd2ed0edd7cee1250ec8142d04509
Rohit Kumar <137387045+rohitkumar9897@users.noreply.github.com>
2023-11-07 02:13:17 +0530

    Time: 3 ms (90.1%), Space: 41.5 MB (96.31%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,21 +1,20 @@
 class Solution {
-    public boolean isAnagram(String s, String t) {
-        if(s.length()!=t.length()){
-            return false;
-        }
-        HashMap <Character, Integer> hm= new HashMap<>();
-        for(char x: s.toCharArray()){
-            hm.put(x, hm.getOrDefault(x,0)+1);
-        }
-        for(char x: t.toCharArray()){
-            hm.put(x, hm.getOrDefault(x,0)-1);
-        }
-
-        for(int v: hm.values()){
-            if(v!=0){
-                return false;
-            }
-        }
-        return true;
+    public boolean isAnagram(String str1, String str2) {
+        // when both of the strings have different lengths
+    if (str1.length() != str2.length())
+      return false;
+ 
+    int[] freq = new int[26];
+    for (int i = 0; i < str1.length(); i++) {
+      freq[str1.charAt(i) - 'a']++;
+    }
+    for (int i = 0; i < str2.length(); i++) {
+      freq[str2.charAt(i) - 'a']--;
+    }
+    for (int i = 0; i < 26; i++) {
+      if (freq[i] != 0)
+        return false;
+    }
+    return true;
     }
 }
