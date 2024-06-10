2a9ef6e5951bf869cb5351a374356cac503cc001
Rohit Kumar <137387045+rohitkumar9897@users.noreply.github.com>
2024-06-11 01:14:34 +0530

    Time: 2 ms (88.81%), Space: 41.3 MB (38.06%) - LeetHub

---
+++ 1137-height-checker/1137-height-checker.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int heightChecker(int[] heights) {
+        int arr[]= heights.clone();
+        Arrays.sort(arr);
+        int ans=0;
+        for(int i=0; i<arr.length; i++){
+            if(arr[i]!=heights[i]){
+                ans++;
+            }
+        }
+        return ans;
+    }
+}
